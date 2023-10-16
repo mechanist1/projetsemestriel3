1adbce985b0d8af2924092a229fe7bf3e4b0c21f
mechanist1 <mlaouhiilyess@gmail.com>
2023-10-16 20:59:45 +0100

    this my first commit without branching, it contains the microservices and its dependencies although some are still missing

---
+++ BusinessLogic/src/test/java/com/example/businesslogic/BusinessLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.businesslogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessLogicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
